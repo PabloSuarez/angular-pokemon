c1362f1b7b80cc8c4babcd70bc4b2a0cfb85746a
Pablo Suarez <hostsuarezpablo@gmail.com>
2015-07-30 03:14:09 -0300

    json

--- public/js/services.js
+++ public/js/services.js
@@ -9,7 +9,7 @@
       function all() {
         var deferred = $q.defer();
 
-        $http.get('./pokemons.json')
+        $http.get('public/pokemons.json')
           .success(function (data) {
             deferred.resolve(data);
           });
